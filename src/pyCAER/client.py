ff59751ce70dafb610d33a5f2b18d276f8a9cedb
Emre Neftci <eneftci@fastmail.fm>
2017-04-14 15:10:32 -0700

    fix sort

--- src/pyCAER/client.py
+++ src/pyCAER/client.py
@@ -112,7 +112,7 @@ class AEDATMonClient(AEDATClientBase):
 
     def _recv_packet(self):
         data_ev_head = self.sock.recv(28)  # we read the header of the packet
-        while data_ev_head<28:
+        while len(data_ev_head)<28:
             data_ev_head += self.sock.recv(28-len(data_ev_head))
             print 'reread'
 
