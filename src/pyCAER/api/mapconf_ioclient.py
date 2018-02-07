12440e02ddf9881b658e0b032d5d38449ba878d8
Emre Neftci <eneftci@uci.edu>
2018-02-07 12:09:51 -0800

    fixed filename problem

--- src/pyCAER/api/mapconf_ioclient.py
+++ src/pyCAER/api/mapconf_ioclient.py
@@ -14,7 +14,8 @@ class Mappings(MappingsBase):
         self.host = host
         self.debug = debug
         self.cur_mappings = []
-        self.filename = '/tmp/mapping_table_' + getuser()
+        self.local_directory = '/tmp/' 
+        self.filename = 'mapping_table_' + getuser()
         #self.clear_mappings()
         super(self.__class__, self).__init__()
 
@@ -43,7 +44,7 @@ class Mappings(MappingsBase):
         src_chip = nsetup.mon.extract_channels(mappings[:,0])
         dst_chip = nsetup.seq.extract_channels(mappings[:,1])
 
-        with open(self.filename, 'w') as f:
+        with open(self.local_directory+self.filename, 'w') as f:
             f.write('#Writing pyNCS Connections')
             for i in range(n_connections):
                 src_chip, dst_chip = nsetup.mon.extract_channels(mappings[i])
@@ -131,7 +132,7 @@ class Mappings(MappingsBase):
     def _commit(self):
         '''
         '''
-        import time
+        import time, ftplib
         conf = None
         for chip in self.neurosetup.chips.values():
             if type(chip.configurator) == Configurator: 
@@ -139,11 +140,15 @@ class Mappings(MappingsBase):
         if conf is None:
             raise Exception('No Dynapse Chip Found\n')
         else:
+            ftp = ftplib.FTP(self.host, user='pyncs')
+            ftp.storlines('STOR {0}'.format(self.filename),
+                    open('{0}'.format(self.local_directory+self.filename),'r'))
+            ftp.close()
             while conf.get_caer_sshs("/netparser/", "ProgramNetworkFrom.txt", 'bool').strip('\x00'.encode()) is "true":
                 print(conf.get_caer_sshs("/netparser/", "ProgramNetworkFrom.txt", 'bool'))
                 print("Waiting other processes to finish weight programming")
                 time.sleep(.5)
-            conf.set_caer_sshs("/netparser/", "net_txt_file", "string", self.filename)
+            conf.set_caer_sshs("/netparser/", "net_txt_file", "string", "/var/opt/pyncs/"+self.filename)
             conf.set_caer_sshs("/netparser/", "ProgramNetworkFrom.txt", 'bool', "true")
             while conf.get_caer_sshs("/netparser/", "ProgramNetworkFrom.txt", 'bool').strip('\x00'.encode()) is "true":
                 print("Waiting weight programming to complete")
