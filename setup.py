eb916b84373286cdb9b7de421af3022c6df7cc18
Emre Neftci <eneftci@uci.edu>
2018-02-08 17:03:41 -0800

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyCAER',
-	version='20180202_r1',
+	version='20180208_r0',
 	description='python Caer client and NCS api',
 	author='Emre Neftci',
 	author_email='eneftci@uci.edu',
